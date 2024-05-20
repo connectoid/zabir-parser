c3fe29f2ad4ea4263069d390699e3d6fd2c4e196
Alexander Beley <beleysasha@gmail.com>
2024-05-20 12:25:23 +1200

    sessions added

--- parser.py
+++ parser.py
@@ -4,7 +4,6 @@ import base64
 import os
 from random import shuffle
 from datetime import datetime
-import asyncio
 
 import requests
 from bs4 import BeautifulSoup
@@ -21,7 +20,7 @@ token = base64.b64encode(credentials.encode())
 header = {'Authorization': 'Basic ' + token.decode('utf-8')}
 
 
-async def create_post(post_json):
+def create_post(post_json):
     url = "https://eskant-foto.ru/wp-json/wp/v2/posts"
     content = create_content(post_json['images'])
     post = {
@@ -32,7 +31,8 @@ async def create_post(post_json):
         # 'fifu_image_alt': 'fifu_image_alt'
         # 'featured_media': 0,
     }
-    response = requests.post(url , headers=header, json=post)
+    with requests.Session() as session:
+        response = session.post(url , headers=header, json=post)
     # print(response.json())
 
 
@@ -62,58 +62,60 @@ def create_content(image_json):
     return content
 
 
-async def get_posts(url):
+def get_posts(url):
     posts_list = []
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'lxml')
-        post_divs = soup.find_all('div', class_='post')
-        post_divs = [div for div in post_divs if div.find('h2')]
-        for post in post_divs:
-            post_dict = {}
-            post_dict['title'] = post.find('h2').find('a').text
-            post_dict['url'] = base_url + post.find('h2').find('a')['href']
-            posts_list.append(post_dict)
-        return posts_list
-    else:
-        print(f'Request Error: {response.status_code}')
-        return False
+    with requests.Session() as session:
+        response = session.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'lxml')
+            post_divs = soup.find_all('div', class_='post')
+            post_divs = [div for div in post_divs if div.find('h2')]
+            for post in post_divs:
+                post_dict = {}
+                post_dict['title'] = post.find('h2').find('a').text
+                post_dict['url'] = base_url + post.find('h2').find('a')['href']
+                posts_list.append(post_dict)
+            return posts_list
+        else:
+            print(f'Request Error: {response.status_code}')
+            return False
     
 
-async def get_images_from_post(url):
+def get_images_from_post(url):
     images_list = []
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'lxml')
-        pictures = soup.find_all('picture')
-        images = [picture.find('img') for picture in pictures]
-        for img in images:
-            image_dict = {}
-            image_dict['alt'] = img['alt']
-            image_dict['title'] = img['title']
-            image_dict['src'] = img['src']
-            try:
-                image_dict['data-src'] = img['data-src']
-            except:
-                image_dict['data-src'] = ''
-            images_list.append(image_dict)
-        return images_list
-    else:
-        print(f'Request Error: {response.status_code}')
-        return False
+    with requests.Session() as session:
+        response = session.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'lxml')
+            pictures = soup.find_all('picture')
+            images = [picture.find('img') for picture in pictures]
+            for img in images:
+                image_dict = {}
+                image_dict['alt'] = img['alt']
+                image_dict['title'] = img['title']
+                image_dict['src'] = img['src']
+                try:
+                    image_dict['data-src'] = img['data-src']
+                except:
+                    image_dict['data-src'] = ''
+                images_list.append(image_dict)
+            return images_list
+        else:
+            print(f'Request Error: {response.status_code}')
+            return False
 
 
-async def main():
+def main():
     time1 = datetime.now()
     count = 0
     max_posts = 50
     posts = []
     post_json = {}
-    posts = await get_posts(base_url)
+    posts = get_posts(base_url)
     shuffle(posts)
     for post in posts:
         post_json['title'] = post['title']
-        images_list = await get_images_from_post(post['url'])
+        images_list = get_images_from_post(post['url'])
         shuffle(images_list)
         images = []
         for image in images_list:
@@ -129,10 +131,10 @@ async def main():
         # if count >= max_posts:
         #     break
 
-        result = await create_post(post_json)
+        result = create_post(post_json)
     time2 = datetime.now()
     print(f'Время парсинга одной страницы: {time2 - time1}')
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
